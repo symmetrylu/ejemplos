24fadf88fd4101b9001e3b1e4f63359d9e372e65
Symmetry Lu <119913043+symmetrylu@users.noreply.github.com>
2022-12-05 22:32:46 -0600

    Create 01_elementos_lista.py

---
+++ 01_elementos_lista.py
@@ -0,0 +1,15 @@
+lista = [32, 'hola', (1,2,3), ['rojo', 'rosa', 'negro']]
+
+# Lista anidada
+print(lista[-1])
+
+# Elementos de la lista anidada
+print(lista[-1][0])
+print(lista[-1][1])
+print(lista[-1][2])
+
+# Elementos de la cadena 'rosa'
+print(lista[-1][1][-4])
+print(lista[-1][1][-3])
+print(lista[-1][1][-2])
+print(lista[-1][1][-1])
